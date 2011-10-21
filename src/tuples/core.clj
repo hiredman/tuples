1f7d9d34186f5076a1e90918e936dcb053a1fbce
Kevin Downey <redchin@gmail.com>
2011-10-20 21:50:08 -0700

    init

---
+++ src/tuples/core.clj
@@ -0,0 +1,223 @@
+(ns tuples.core
+  (:import (clojure.lang IPersistentVector
+                         Associative
+                         Sequential
+                         IPersistentStack
+                         Reversible
+                         Indexed
+                         IFn
+                         Counted
+                         Util
+                         IObj
+                         IMeta
+                         IPersistentCollection
+                         ILookup
+                         Seqable
+                         IMapEntry
+                         RT)
+           (java.util RandomAccess List Collection
+                      Map$Entry)
+           (java.io Serializable)))
+
+(defmulti
+  ^{:inline (fn [& args]
+              (let [class-name (symbol (format "tuples.core.Tuple%s"
+                                               (count args)))]
+                `(new ~class-name ~@args {})))}
+  tuple (comp count list))
+
+(defprotocol TupleAccess
+  (get0 [t])
+  (get1 [t])
+  (get2 [t])
+  (get3 [t])
+  (get4 [t])
+  (get5 [t])
+  (get6 [t])
+  (get7 [t])
+  (get8 [t])
+  (get9 [t]))
+
+(defmacro tuple-for [n]
+  (let [class-name (symbol (format "Tuple%s" n))
+        fields (into {} (for [i (range n)]
+                          [i (symbol (format "e%s" (inc i)))]))
+        low-end (dec n)
+        high-end (inc n)]
+    `(do
+       (deftype ~class-name [~@(map second (sort-by first fields)) md#]
+         ~@(when (= n 2)
+             `[IMapEntry
+               (key [v#]
+                    ~(get fields 0))
+               (val [v#]
+                    ~(get fields 1))
+               Map$Entry
+               (getKey [v#]
+                       ~(get fields 0))
+               (getValue [v#]
+                         ~(get fields 1))
+               (setValue [v# _#]
+                         (throw (UnsupportedOperationException.)))])
+         TupleAccess
+         ~@(for [i (range 10)]
+             `(~(symbol (str "get" i)) [~'t]
+               ~(if (> i n)
+                  `(throw (UnsupportedOperationException.))
+                  (get fields i))))
+         Associative
+         (containsKey [v# key#]
+           (if (and (Util/isInteger key#)
+                    (> ~(inc n) key# ~(dec n)))
+             true
+             false))
+         (entryAt [v# key#]
+           (if (Util/isInteger key#)
+             (let [key# (int key#)]
+               (case key#
+                     ~@(for [[idx n] fields
+                             itm [idx [idx n]]]
+                         itm)
+                     nil))
+             nil))
+         (assoc [v# key# val#]
+           (.assocN v# key# val#))
+         Sequential ; Marker
+         IPersistentStack
+         (peek [v#]
+           ~(last (sort-by first fields)))
+         (pop [v#]
+           (throw (UnsupportedOperationException.)))
+         Reversible
+         (rseq [v#]
+           (throw (UnsupportedOperationException.)))
+         Indexed
+         (nth [v# i#]
+           (.nth v# i# nil))
+         (nth [v# i# not-found#]
+           (if (Util/isInteger i#)
+             (let [i# (int i#)]
+               (case i#
+                     ~@(for [[idx n] fields
+                             itm [idx n]]
+                         itm)
+                     not-found#))
+             not-found#))
+         Counted
+         (count [v#] ~n)
+         IPersistentVector
+         (length [v#] ~n)
+         (assocN [v# i# val#]
+           (throw (UnsupportedOperationException.)))
+         (cons [v# o#]
+           (throw (UnsupportedOperationException.)))
+         IFn
+         (invoke [v# arg1#]
+           (let [arg1# (int arg1#)]
+             (if (Util/isInteger arg1#)
+               (case arg1#
+                     ~@(for [[idx n] fields
+                             itm [idx n]]
+                         itm)
+                     nil)
+               (throw (IllegalArgumentException. "Key must be integer")))))
+         IObj
+         (withMeta [v# m#]
+           (throw (UnsupportedOperationException.)))
+         IMeta
+         (meta [v#] md#)
+         IPersistentCollection
+         (empty [v#] [])
+         (equiv [v1# v2#]
+           (if (instance? Sequential v2#)
+             (if (= (count v2#) ~n)
+               (zero? (.compareTo v1# v2#))
+               false)
+             false))
+         Seqable
+         (seq [v#]
+           (list ~@(map second (sort-by first fields))))
+         ILookup
+         (valAt [v# key#]
+           (.nth v# key# nil))
+         (valAt [v# key# not-found#]
+           (.nth v# key# not-found#))
+         ;;JAVA
+         RandomAccess ; Marker
+         List
+         (lastIndexOf [v# ~'obj]
+           (cond
+            ~@(for [i (range (dec n) 0 -1)
+                    itm [`(= ~(get fields i) ~'obj) i]]
+                itm)
+            :else -1))
+         (subList [v# fidx# tidx#]
+           (throw (UnsupportedOperationException.)))
+         (set [v# idx# o#]
+           (throw (UnsupportedOperationException.)))
+         (listIterator [v#]
+           (throw (UnsupportedOperationException.)))
+         (listIterator [v# idx#]
+           (throw (UnsupportedOperationException.)))
+         (add [v# obj# idx#]
+           (throw (UnsupportedOperationException.)))
+         (get [v# idx#]
+           (.nth v# idx#))
+         (indexOf [v# ~'obj]
+           (cond
+            ~@(for [i (range n)
+                    itm [`(= ~(get fields i) ~'obj) i]]
+                itm)
+            :else -1))
+         Collection
+         (isEmpty [v#] false)
+         (contains [v# k#]
+           (and (Util/isInteger k#)
+                (> ~(inc n) (long k#))
+                (> (long k#) -1)))
+         (size [v#]
+           ~n)
+         (toArray [v#]
+           (RT/seqToArray (.seq v#)))
+         (toArray [v# obj#]
+           (throw (UnsupportedOperationException.)))
+         (addAll [v# collection#]
+           (throw (UnsupportedOperationException.)))
+         (iterator [v#]
+           (.iterator (.seq v#)))
+          (removeAll [v# c#]
+            (throw (UnsupportedOperationException.)))
+          (retainAll [v# c#]
+            (throw (UnsupportedOperationException.)))
+         (add [v# obj#]
+           (throw (UnsupportedOperationException.))) 
+         (clear [v#]
+           (throw (UnsupportedOperationException.)))
+         Comparable
+         (compareTo [v1# v2#]
+           (let [^IPersistentVector v2# v2#]
+             (cond
+              (> (count v2#) ~n)
+              -1
+              (< (count v2#) ~n)
+              1
+              :else
+              (loop [i# 0]
+                (if (> ~n i#)
+                  (let [c# (Util/compare (.nth v1# i#) (.nth v2# i#))]
+                    (if (zero? c#)
+                      (recur (inc i#))
+                      c#))
+                  0)))))
+         Serializable ; Marker
+         )
+       (defmethod tuple ~n [~@(map second(sort-by first fields))]
+         (new ~class-name ~@(map second(sort-by first fields)) {})))))
+
+(defmacro generate-tuples []
+  `(do
+     ~@(for [i (range 10)]
+         `(tuple-for ~i))))
+
+(generate-tuples)
+
