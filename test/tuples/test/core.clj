40fb4f76b593cc12ca0eb9b83b1b86fb86fcc71f
Kevin Downey <redchin@gmail.com>
2011-10-20 23:24:51 -0700

    some tests

--- test/tuples/test/core.clj
+++ test/tuples/test/core.clj
@@ -2,5 +2,19 @@
   (:use [tuples.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest t-tuples
+  (doseq [elements (take-while seq (iterate pop (vec (range 0 10))))
+          :let [tup (apply tuple elements)]
+          element elements]
+    (are [x y] (= x y)
+         element ((resolve (symbol (str "get" element))) tup)
+         [element element] (.entryAt tup element))
+    (if (zero? (count tup))
+      (is (empty? tup))
+      (is (not (empty? tup)))))
+  (is (= [:a :b] (tuple :a :b)))
+  (is (= 1 (.get (tuple 4 5 3 1 2) 3)))
+  (let [x {:a 1}
+        tup (tuple 1)]
+    (is (= {} (meta tup)))
+    (is (= x (meta (with-meta tup x))))))
