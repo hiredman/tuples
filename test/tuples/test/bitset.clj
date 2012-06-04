81cee95430ff577131351a731594cd657a216509
Kevin Downey <redchin@gmail.com>
2012-06-04 11:31:10 -0700

    simple tests

---
+++ test/tuples/test/bitset.clj
@@ -0,0 +1,20 @@
+(ns tuples.test.bitset
+  (require [tuples.bitset :as bs]
+           [clojure.test :refer :all]))
+
+(deftest t-bitset
+  (let [bs (bs/bitset)]
+    (is (= 64 (bs/capacity bs)))
+    (is (= 1 (count bs)))
+    (is (= 3 (count (bs/set bs 128))))
+    (is (= (str (reduce #(bs/set %1 %2)
+                        (bs/bitset)
+                        (range 128)))
+           "0xffffffffffffffff 0xffffffffffffffff"))
+    (is (= true (bs/get (bs/set bs 1) 1)))
+    (is (= false (bs/get bs 1)))))
+
+(deftest t-get-set
+  (let [bs (bs/bitset)]
+    (dotimes [i 500]
+      (is (= true (bs/get (bs/set bs i) i))))))
